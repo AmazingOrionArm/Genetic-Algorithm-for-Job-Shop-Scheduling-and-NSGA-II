1d7f8243343d954ec0d4d75f9e9ffcec4bc77998
AmazingOrionArm <s990401@gmail.com>
2019-05-27 16:58:34 +0800

    Update GA_JobShop_Scheduler.py

--- Implementation with python ( OOP style version )/GA-jobshop/GA_JobShop_Scheduler.py
+++ Implementation with python ( OOP style version )/GA-jobshop/GA_JobShop_Scheduler.py
@@ -1,3 +1,12 @@
+"""
+===== Original Version Code: https://github.com/wurmen/Genetic-Algorithm-for-Job-Shop-Scheduling-and-NSGA-II/blob/master/implementation%20with%20python/GA-jobshop/GA_jobshop_makespan.py
+===== Original Author's Information =====
+Original Author: cheng-man wu
+LinkedIn: www.linkedin.com/in/chengmanwu
+Github: https://github.com/wurmen
+=====
+"""
+
 import numpy as np
 import pandas as pd
 
@@ -9,7 +18,7 @@ class Scheduler:
             "processTimeTable": pd.DataFrame(),
             "machinesSequenceTable": pd.DataFrame(),
             # basic parameters
-            "populationRate": 30,
+            "populationSize": 30,
             "crossoverRate": 0.8,
             "mutationRate": 0.8,
             "mutationSelectionRate": 0.8,
@@ -36,4 +45,57 @@ class Scheduler:
         # 一般數執行參數則直接assign
         else:
             self.__parameters[preferParameter] = preferValue
+    
+    # 啟動函式，部分原碼暫時沿用原版本，待後續視情況修改
+    def Run(self):
+        print("Calculating...")
+        
+        dfshape=self.__parameters["processTimeTable"].shape
+        num_mc=dfshape[1] # number of machines
+        num_job=dfshape[0] # number of jobs
+        num_gene=num_mc*num_job # number of genes in a chromosome
+
+        pt=[list(map(int, pt_tmp.iloc[i])) for i in range(num_job)]
+        ms=[list(map(int,ms_tmp.iloc[i])) for i in range(num_job)]
+        
+        Tbest=999999999999999
+        best_list,best_obj=[],[]
+        population_list=[]
+        makespan_record=[]
+        for t in range(self.__parameters["populationSize"]):
+            print("=== Generation:" + str(t) + "===")
+            nxm_random_num=list(np.random.permutation(num_gene)) # generate a random permutation of 0 to num_job*num_mc-1
+            population_list.append(nxm_random_num) # add to the population_list
+            for j in range(num_gene):
+                population_list[i][j]=population_list[i][j]%num_job # convert to job number format, every job appears m times
+    
+    def NewPupulation(self):
+        # 產生初始群體
+        
+    def ReProduction(self):
+        # 產生新群體
+        
+    def Selection(self):
+        # 選擇
+        
+    def Crossover(self):
+        # 交配
+        
+    def Mutation(self):
+        # 突變
+        
+    def Repair(self):
+        # 修復
+        
+    def CalcFitness(self, c):
+        # 計算適應值
+        
+    def PrintResult(self):
+        # 印出計算結果
+        
+    def PrintFitnessPlot(self):
+        # 繪製適應度趨勢圖
+    
+    def GenerateGanttChart(self):
+        # 繪製甘特圖
         
